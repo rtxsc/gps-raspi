e4407929a41c1eab89135d7494dd23c04c26820e
rtxsc <robotronix.uitm@gmail.com>
2022-07-06 14:32:09 +0800

    Create gps_sim7000.py

---
+++ gps_sim7000.py
@@ -0,0 +1,14 @@
+"""
+sudo screen /dev/ttyUSB2 115200
+
+"""
+
+from time import sleep
+try:
+    import git_python_handler
+except:
+    pass
+
+for i in range(0,100,20):
+    print("GPS TEST:{}".format(i))
+    sleep(1)
